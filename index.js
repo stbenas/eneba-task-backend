19e45137f1b7d3347836245b2ba4ff9f94f165f5
Benas Stanulis <benas@pickit.lt>
2025-12-28 16:28:38 +0200

    Changed index.js to include /product routed via routes/product.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import searchRoutes from "./routes/search.js";
+import productRoutes from "./routes/product.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use("/product", productRoutes);
 app.use("/", searchRoutes);
 
 app.listen(PORT, () => {
