8ce8294c53811412b3353fd89650a061eb044dd7
Benas Stanulis <benas@pickit.lt>
2025-12-28 20:37:34 +0200

    Change CORS policy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import productRoutes from "./routes/product.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 function invalidJsonHandler(err, req, res, next) {
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
@@ -17,6 +17,20 @@ function invalidJsonHandler(err, req, res, next) {
 
   next(err);
 }
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 
 app.use(express.json());
 app.use(invalidJsonHandler);
