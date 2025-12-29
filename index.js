35911928a0e1f15ec4af959d407899f48a13e9a4
Benas Stanulis <benas@pickit.lt>
2025-12-29 03:17:59 +0200

    Update CORS configuration to use environment variable for origin

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function invalidJsonHandler(err, req, res, next) {
 }
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
